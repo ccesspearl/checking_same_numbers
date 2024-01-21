42b746872b4af0413edcafc23ec1e56a93dee852
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-22 05:47:22 +0800

    created a check function

--- checking_same_numbers.py
+++ checking_same_numbers.py
@@ -1,9 +1,11 @@
 # Exercise 5: Check if the first and last number of a list is the same
 # Write a function to return True if the first and last number of a given list is same. If numbers are different then return False. 
 
-#Pseudocode 
+# Pseudocode 
 
 # Create a check function 
+def check(given_list): 
+
 # Inside the function, create a print syntax that will show the given list 
 # Inside the function, use the return syntax 
 # Create a variable to check the list 
