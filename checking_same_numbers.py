4224d46eb0fc064f51ef6e85bfac6e2de439b90b
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-22 05:45:58 +0800

    pseudocode

--- checking_same_numbers.py
+++ checking_same_numbers.py
@@ -1,2 +1,12 @@
 # Exercise 5: Check if the first and last number of a list is the same
 # Write a function to return True if the first and last number of a given list is same. If numbers are different then return False. 
+
+#Pseudocode 
+
+# Create a check function 
+# Inside the function, create a print syntax that will show the given list 
+# Inside the function, use the return syntax 
+# Create a variable to check the list 
+# Print the result 
+
+
