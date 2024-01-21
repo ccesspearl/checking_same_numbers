9238bbb274b973d95ffd5fe8d031c61e8bcfe663
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-22 05:49:06 +0800

    created print syntax

--- checking_same_numbers.py
+++ checking_same_numbers.py
@@ -7,6 +7,8 @@
 def check(given_list): 
 
 # Inside the function, create a print syntax that will show the given list 
+    print("Given list:", given_list)
+
 # Inside the function, use the return syntax 
 # Create a variable to check the list 
 # Print the result 
